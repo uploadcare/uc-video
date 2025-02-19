e9789d37fa43b545e0d32678287e4af51cb5432e
Egor Didenko <didenk.egor@gmail.com>
2025-02-19 10:54:38 -0500

    chore(ci): added steps in the ci

--- release.config.cjs
+++ release.config.cjs
@@ -7,12 +7,19 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "git checkout -b release/${nextRelease.version} && git push origin release/${nextRelease.version}"
+      }
+    ],
     "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
         assets: ["./package.json", "./CHANGELOG.md"],
         message: "chore(release): ${nextRelease.version} [skip ci]",
+        push: false
       }
     ],
     "@semantic-release/github"
